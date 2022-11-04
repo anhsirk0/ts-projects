9d50b56d8fb3a135cb6aa73938d5e2fccba92345
krishna <krishna404@yandex.com>
2022-11-04 19:37:30 +0530

    Updated index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,10 +24,10 @@ const Home: NextPage = () => {
         <p className="text-xl md:text-3xl text-center text-white">
           Some React + TS components.
         </p>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-6 max-w-7xl">
           {links.map((link) => (
             <Link className="ring-rose-500" key={link.url} href={link.url}>
-              <a className="flex items-center justify-center px-8 h-20 md:h-24 lg:h-28 bg-white rounded-2xl shadow cursor-pointer hover:bg-opacity-95 hover:shadow-lg">
+              <a className="flex items-center justify-center px-4 h-20 md:h-24 lg:h-32 bg-white rounded-2xl shadow cursor-pointer hover:bg-opacity-95 hover:shadow-lg">
                 <span className="text-2xl md:text-3xl text-center text-gray-700">
                   {link.title}
                 </span>
